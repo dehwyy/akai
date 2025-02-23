9eb902d31edd5caba3b01182d4035a99840bae8c
dehwyy <dehwyy@yandex.ru>
2025-02-23 23:06:40 +0300

    [example!]: demo app

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include "deps/imgui/sfml.hpp"
-#include "imgui.h"
+#include "deps/imgui-sfml/imgui-sfml.hpp"
+#include "deps/imgui-sfml/imgui.h"
 
 #include <cmath>
 #include <iostream>
@@ -7,56 +7,66 @@
 #include "SFML/Graphics.hpp"
 #include "nlohmann/json.hpp"
 
-// using json = nlohmann::json;
+using json = nlohmann::json;
 
-// const int WIDTH = 800;         // Ширина окна
-// const int HEIGHT = 600;        // Высота окна
-// const float SCALE_X = 50.0f;   // Масштаб по X (увеличение)
-// const float SCALE_Y = 100.0f;  // Масштаб по Y (увеличение)
+const int WIDTH = 640;         // Ширина окна
+const int HEIGHT = 480;        // Высота окна
+const float SCALE_X = 50.0f;   // Масштаб по X (увеличение)
+const float SCALE_Y = 100.0f;  // Масштаб по Y (увеличение)
 
-// const double EPSILON = 0.1;
+const double EPSILON = 0.1;
 
-// double f(double x) {
-//     double radian = std::fmod(x, 2. * 3.14);
+double f(double x) {
+    double radian = std::fmod(x, 2. * 3.14);
 
-//     std::cout << x << " " << radian << std::endl;
-//     const double breakpoin = radian > 3.14 ? 3. * 3.14 / 2. : 3.14 / 2.;
-//     if (radian > breakpoin - EPSILON && radian <= breakpoin + EPSILON) {
-//         const double borderX = breakpoin - ((breakpoin - radian) > 0 ? -EPSILON : EPSILON);
-//         return f(borderX);
-//     }
+    // std::cout << x << " " << radian << std::endl;
+    const double breakpoin = radian > 3.14 ? 3. * 3.14 / 2. : 3.14 / 2.;
+    if (radian > breakpoin - EPSILON && radian <= breakpoin + EPSILON) {
+        const double borderX = breakpoin - ((breakpoin - radian) > 0 ? -EPSILON : EPSILON);
+        return f(borderX);
+    }
 
-//     return std::fabs(std::cos(radian));
-// }
+    return std::fabs(std::cos(radian));
+}
 
-// void drawGraph(sf::RenderWindow* window, double k) {
-//     std::cout << "k = " << k << std::endl;
-//     sf::VertexArray graph(sf::LineStrip);
+void drawGraph(sf::RenderWindow* window, double k) {
+    // std::cout << "k = " << k << std::endl;
+    sf::VertexArray graph(sf::LineStrip);
 
-//     for (float x = -8.0 * std::fabs(1 / k); x <= 8.0 * std::fabs(1 / k);
-//          x += 0.1) {            // Диапазон X от -8 до 8
-//         float y = std::sin(x);  // Функция
+    for (float x = -8.0 * std::fabs(1 / k); x <= 8.0 * std::fabs(1 / k);
+         x += 0.1) {            // Диапазон X от -8 до 8
+        float y = std::sin(x);  // Функция
 
-//         // Преобразование координат в пиксели SFML
-//         float pixelX = WIDTH / 2 + x * SCALE_X * k;
-//         float pixelY = HEIGHT / 2 - y * SCALE_Y;
+        // Преобразование координат в пиксели SFML
+        float pixelX = WIDTH / 2 + x * SCALE_X * k;
+        float pixelY = HEIGHT / 2 - y * SCALE_Y;
 
-//         graph.append(sf::Vertex(sf::Vector2f(pixelX, pixelY), sf::Color::Magenta));
-//     }
+        graph.append(sf::Vertex(sf::Vector2f(pixelX, pixelY), sf::Color::Magenta));
+    }
 
-//     window->draw(graph);
-// }
+    window->draw(graph);
+}
 
 void imgui() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
-    window.setFramerateLimit(60);
-    std::cout << ImGui::SFML::Init(window) << std::endl;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Akai", sf::Style::Default);
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::Image appIcon;
+    appIcon.loadFromFile("joker1.jpg");
+    auto [iconWidth, iconHeight] = appIcon.getSize();
+
+    window.setIcon(iconWidth, iconHeight, appIcon.getPixelsPtr());
+    window.setFramerateLimit(60);
+
+    if (!ImGui::SFML::Init(window)) {
+        exit(1);
+    }
+
+    ImGui::Spectrum::StyleColorsSpectrum();
 
     sf::Clock deltaClock;
+    unsigned counter = 0;
     while (window.isOpen()) {
+        counter++;
         sf::Event event;
         while (window.pollEvent(event)) {
             ImGui::SFML::ProcessEvent(window, event);
@@ -65,17 +75,33 @@ void imgui() {
                 window.close();
             }
         }
-
         ImGui::SFML::Update(window, deltaClock.restart());
+        window.clear(sf::Color::Transparent);
 
-        ImGui::ShowDemoWindow();
+        ImGui::Begin("Settings");
+        static float k = 1;
+        if (ImGui::TreeNode("Function")) {
+            ImGui::SliderFloat("K", &k, 0.01, 2.0);
 
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
+            ImGui::TreePop();
+        }
+        // Colored button
+        for (int i = 0; i < 7; i++) {
+            if (i > 0) {
+                ImGui::SameLine();
+            }
+            ImGui::PushID(i);
+            ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)ImColor::HSV(i / 7.0f, 0.6f, 0.6f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, (ImVec4)ImColor::HSV(i / 7.0f, 0.7f, 0.7f));
+            ImGui::PushStyleColor(ImGuiCol_ButtonActive, (ImVec4)ImColor::HSV(i / 7.0f, 0.8f, 0.8f));
+            ImGui::Button("Click");
+            ImGui::PopStyleColor(3);
+            ImGui::PopID();
+        }
+
+        drawGraph(&window, k * f(1));
+
         ImGui::End();
-
-        window.clear();
-        window.draw(shape);
         ImGui::SFML::Render(window);
         window.display();
     }
@@ -141,9 +167,6 @@ int main(int, char**) {
     //             std::cout << "Key pressed: " << event.key.scancode << std::endl;
     //         }
     //     }
-
-    //     // Clear screen
-    //     // window->clear();
 
     //     window->clear(sf::Color::Transparent);
     //     // Draw the sprite
